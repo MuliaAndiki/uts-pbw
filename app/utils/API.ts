cfefeb665d44429f6f806afa9cf8d707f643c995
Mulia Andiki <mulia23@mhs.usk.ac.id>
2025-04-12 14:40:18 +0700

    feat:setUp Axios

--- app/utils/API.ts
+++ app/utils/API.ts
@@ -1,1 +1,25 @@
-import axios from "axios";
+import axios, { AxiosResponse, AxiosError } from "axios";
+
+const API = axios.create({
+  baseURL: "https://api-todo-list-pbw.vercel.app",
+});
+
+API.interceptors.request.use(
+  (config: any): any => {
+    return config;
+  },
+  (error: AxiosError): Promise<AxiosError> => {
+    return Promise.reject(error);
+  }
+);
+
+API.interceptors.response.use(
+  (reponse: AxiosResponse): AxiosResponse => {
+    return reponse;
+  },
+  (error: AxiosError): Promise<AxiosError> => {
+    return Promise.reject(error);
+  }
+);
+
+export default API;
