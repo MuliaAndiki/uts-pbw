cc5b93f78eb266aa5e7029d173311d36d47a951a
Mulia Andiki <mulia23@mhs.usk.ac.id>
2025-04-12 14:21:41 +0700

    feat: installation all

---
+++ app/(pages)/register/page.tsx
@@ -0,0 +1,8 @@
+const Register = () => {
+  return (
+    <div>
+      <h1> ini page Register</h1>
+    </div>
+  );
+};
+export default Register;
---
+++ app/(pages)/todo/page.tsx
@@ -0,0 +1,8 @@
+const Todo = () => {
+  return (
+    <div>
+      <h1>ini page Todo</h1>
+    </div>
+  );
+};
+export default Todo;
